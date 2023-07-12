d28eb6ec6662ee922f56bdee5cf094081431ce6b
tribaaldos <tribaaldos@gmail.com>
2023-07-13 01:08:14 +0200

    model comment schema done

--- models/post.js
+++ models/post.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    comment: {type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+})
 const postSchema = new Schema({
 
     title: {type: String, required: true},
     text: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    comments: [commentSchema]
 }, {
     timestamps: true
 })
